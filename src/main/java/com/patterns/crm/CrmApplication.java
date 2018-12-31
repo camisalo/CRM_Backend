fcd743f7deb0f9005b0e04f20bbc70445b1e057c
Camisalo <konrad.gebczynski@gmail.com>
2018-12-31 13:12:04 +0100

    delete user authorization

--- src/main/java/com/patterns/crm/CrmApplication.java
+++ src/main/java/com/patterns/crm/CrmApplication.java
@@ -3,20 +3,18 @@ package com.patterns.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CrmApplication {
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
     }
-
 }
-
--- src/main/java/com/patterns/crm/users/ApplicationUserRepository.java
+++ src/main/java/com/patterns/crm/users/ApplicationUserRepository.java
@@ -1,7 +1,7 @@
-package com.patterns.crm.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
-    ApplicationUser findByUsername(String username);
-}
+//package com.patterns.crm.users;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+//    ApplicationUser findByUsername(String username);
+//}
