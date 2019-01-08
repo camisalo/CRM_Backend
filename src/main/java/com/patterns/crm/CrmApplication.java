0d94954e901ed0c71f52feaaf1e258ab1f6bf3e4
Camisalo <konrad.gebczynski@gmail.com>
2019-01-08 13:12:36 +0100

    add user authorization

--- src/main/java/com/patterns/crm/CrmApplication.java
+++ src/main/java/com/patterns/crm/CrmApplication.java
@@ -2,6 +2,9 @@ package com.patterns.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,4 +20,15 @@ public class CrmApplication {
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
     }
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
 }
--- src/main/java/com/patterns/crm/controllers/CRMController.java
+++ src/main/java/com/patterns/crm/controllers/CRMController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/crm")
 public class CRMController {
